38a38edac26cf0b1ee9e7095b82d5b0eba6fdd86
Buket <buket-cakir@hotmail.com>
2023-08-26 14:43:06 +0300

    260823-JSBasics

---
+++ Week03/230826/Javascript/01-Basics/scripts/01-Intro.js
@@ -0,0 +1,8 @@
+function sayBye(){
+    alert("Bye Bye!");
+    console.log("Bye Bye!");
+}
+function showAge() {
+    alert("You are 24 years old2.");
+    console.log("Your are 24 years old2.")
+};
