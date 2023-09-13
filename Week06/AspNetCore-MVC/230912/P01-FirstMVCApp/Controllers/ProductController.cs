2d00474f0c1a2554f61247bc1fc4eaaad0c7af7e
Buket <buket-cakir@hotmail.com>
2023-09-13 21:46:07 +0300

    FirstMVCApp

---
+++ Week06/AspNetCore-MVC/230912/P01-FirstMVCApp/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace P01_FirstMVCApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // http://localhost:5214/product/details/2
+        public IActionResult Details(int id)
+        {
+
+            return View(id);
+        }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
